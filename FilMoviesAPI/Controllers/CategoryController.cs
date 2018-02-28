7ecea6cb2dac62075f919904fc4cc042fc17bbf4
Letícia Domingues <leticia.domingues@opus-software.com.br>
2018-02-28 13:16:31 -0300

    Requests - getCategory, getAllCategories

---
+++ FilMoviesAPI/Controllers/CategoryController.cs
@@ -0,0 +1,50 @@
+﻿using FilMoviesAPI.Model;
+using FilMoviesAPI.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FilMoviesAPI.Controllers
+{
+    public class CategoryController : ApiController
+    {
+        public HttpResponseMessage Get(int id)
+        {
+            using (var unityOfWork = new UnitOfWork(new FilMoviesContext()))
+            {
+                try
+                {
+                    Category category = unityOfWork.Categories.Get(id);
+                    return Request.CreateResponse(HttpStatusCode.OK, category);
+                }
+                catch (KeyNotFoundException)
+                {
+                    var mensagem = string.Format("A categoria {0} não foi encontrado.", id);
+                    var error = new HttpError(mensagem);
+                    return Request.CreateResponse(HttpStatusCode.NotFound, error);
+                }
+            }
+        }
+
+        public HttpResponseMessage Get()
+        {
+            using (var unityOfWork = new UnitOfWork(new FilMoviesContext()))
+            {
+                try
+                {
+                    ICollection<Category> categories = (ICollection<Category>)unityOfWork.Categories.GetAll();
+                    return Request.CreateResponse(HttpStatusCode.OK, categories);
+                }
+                catch (KeyNotFoundException)
+                {
+                    var mensagem = string.Format("Erro");
+                    var error = new HttpError(mensagem);
+                    return Request.CreateResponse(HttpStatusCode.NotFound, error);
+                }
+            }
+        }
+    }
+}
